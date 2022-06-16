a27f43d7e4a78ff0597171ef6d1148e4deb8d5b5
Hamza EL Yousfi <Hamza.El_Yousfi@bordeaux-inp.fr>
2022-06-16 14:00:08 +0200

    get the return values of threads

--- threads.py
+++ threads.py
@@ -31,3 +31,26 @@ t2.join()
 end_time = perf_counter()
 
 print(f'It took {end_time- start_time: 0.2f} second(s) to complete.')
+
+
+
+"""
+# To get the return value back store the results in a database
+from threading import Thread
+result = {}
+
+def foo(bar):
+    print('hello {}'.format(bar))
+    result[str(bar)] = bar
+
+thread1 = Thread(target=foo, args=('world!',))
+thread2 = Thread(target=foo, args=('wold2!',))
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+print(result)
+"""
