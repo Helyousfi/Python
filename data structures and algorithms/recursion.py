aeb3b3dafbd8f9e289292df1801e983c7cee63c0
Hamza EL Yousfi <Hamza.El_Yousfi@bordeaux-inp.fr>
2022-07-11 10:27:38 +0200

    Update recursion.py

--- data structures and algorithms/recursion.py
+++ data structures and algorithms/recursion.py
@@ -4,6 +4,20 @@ def factorial(n):
         return 1
     else:
         return n*factorial(n-1)
+    
+def reverseString(str):
+    if(len(str)!=0):
+        reverseString(str[:-1])
+        print(str[-1])
+        
 
+# v(i, j) = v(i - 1, j - 1) + v(i - 1, j) 
+def pascal(i, j):
+    if(j==1 or i==j):
+        return 1
+    else:
+        return pascal(i-1,j-1) + pascal(i-1,j)
+
+    
 if __name__ == "__main__":
     print(factorial(4))
